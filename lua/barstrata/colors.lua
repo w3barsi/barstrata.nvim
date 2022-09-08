4829866e5fbce7b24465b8365bdbeaac3158cea1
darcybalaga@gmail.com <darcybalaga@gmail.com>
2022-09-08 16:13:22 +0800

    colors and lua

---
+++ lua/barstrata/colors.lua
@@ -0,0 +1,83 @@
+local M = {}
+
+M.default = {
+  -- Basic
+  bg = "#191c25",
+  fg = "#ffffff",
+  -- Normal
+  --
+  --
+  black = "#4b4e57",
+  red = "#fe9f7c",
+  green = "#92c47e",
+  yellow = "#d2b45f",
+  blue = "#a0b9d8",
+  magenta = "#c6aed7",
+  cyan = "#7dc2c7",
+  white = "#ffffff",
+  -- Bright
+  bright_black = "#4b4e57",
+  bright_red = "#fe9f7c",
+  bright_green = "#92c47e",
+  bright_yellow = "#d2b45f",
+  bright_blue = "#a0b9d8",
+  bright_magenta = "#c6aed7",
+  bright_cyan = "#7dc2c7",
+  bright_white = "#ffffff",
+  -- Grays
+  gray01 = "#20222d",
+  gray02 = "#272935",
+  gray03 = "#2e313d",
+  gray04 = "#3c3f4e",
+  gray05 = "#494c5a",
+  gray06 = "#6c6f82",
+  gray07 = "#85889b",
+  -- Special
+  urgent_red = "#ff6961",
+  urgent_yellow = "#fdfd96",
+  urgent_green = "#7FEB7F",
+  urgent_blue = "#91DDF2",
+  none = "NONE",
+}
+
+M.black = {
+  -- Basic
+  bg = "#000000",
+  fg = "#ffffff",
+  -- Normal
+  --
+  --
+  black = "#4b4e57",
+  red = "#fe9f7c",
+  green = "#92c47e",
+  yellow = "#d2b45f",
+  blue = "#a0b9d8",
+  magenta = "#c6aed7",
+  cyan = "#7dc2c7",
+  white = "#ffffff",
+  -- Bright
+  bright_black = "#4b4e57",
+  bright_red = "#fe9f7c",
+  bright_green = "#92c47e",
+  bright_yellow = "#d2b45f",
+  bright_blue = "#a0b9d8",
+  bright_magenta = "#c6aed7",
+  bright_cyan = "#7dc2c7",
+  bright_white = "#ffffff",
+  -- Grays
+  gray01 = "#20222d",
+  gray02 = "#272935",
+  gray03 = "#2e313d",
+  gray04 = "#3c3f4e",
+  gray05 = "#494c5a",
+  gray06 = "#6c6f82",
+  gray07 = "#85889b",
+  -- Special
+  urgent_red = "#ff6961",
+  urgent_yellow = "#fdfd96",
+  urgent_green = "#7FEB7F",
+  urgent_blue = "#91DDF2",
+  none = "NONE",
+}
+
+return M
