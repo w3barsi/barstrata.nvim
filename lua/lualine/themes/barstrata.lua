c935c605e97ecbe333b24ce936ff870251a8564b
w3barsi <darcybalaga@gmail.com>
2022-10-15 00:17:39 +0800

    fixed lualine command mode colors

--- lua/lualine/themes/barstrata.lua
+++ lua/lualine/themes/barstrata.lua
@@ -5,35 +5,34 @@ local c = variants[cfg.variant]
 local barstrata = {}
 
 barstrata.normal = {
-	a = { fg = c.bg, bg = c.blue, gui = 'bold' },
-	b = { fg = c.text, bg = c.background1 },
-	c = { fg = c.blueGray1, bg = c.none },
+  a = { fg = c.bg, bg = c.blue, gui = "bold" },
+  b = { fg = c.text, bg = c.background1 },
+  c = { fg = c.blueGray1, bg = c.none },
 }
 
 barstrata.insert = {
-	a = { fg = c.bg, bg = c.teal, gui = 'bold' },
-	b = { fg = c.text, bg = c.background1 },
+  a = { fg = c.bg, bg = c.teal, gui = "bold" },
+  b = { fg = c.text, bg = c.background1 },
 }
 
 barstrata.visual = {
-	a = { fg = c.bg, bg = c.bright_pink, gui = 'bold' },
-	b = { fg = c.text, bg = c.background1 },
+  a = { fg = c.bg, bg = c.bright_pink, gui = "bold" },
+  b = { fg = c.text, bg = c.background1 },
 }
 
 barstrata.replace = {
-	a = { fg = c.background2, bg = c.pink3, gui = 'bold' },
-	b = { fg = c.text, bg = c.background1 },
-
+  a = { fg = c.bg, bg = c.pink3, gui = "bold" },
+  b = { fg = c.text, bg = c.background1 },
 }
 barstrata.command = {
-	a = { fg = c.background2, bg = c.yellow, gui = 'bold' },
-	b = { fg = c.text, bg = c.background1 },
+  a = { fg = c.bg, bg = c.yellow, gui = "bold" },
+  b = { fg = c.text, bg = c.background1 },
 }
 
 barstrata.inactive = {
-	a = { fg = c.blueGray3, bg = c.background1, gui = 'bold' },
-	b = { fg = c.blueGray3, bg = c.background1 },
-	c = { fg = c.blueGray3, bg = c.none },
+  a = { fg = c.blueGray3, bg = c.background1, gui = "bold" },
+  b = { fg = c.blueGray3, bg = c.background1 },
+  c = { fg = c.blueGray3, bg = c.none },
 }
 
 return barstrata
