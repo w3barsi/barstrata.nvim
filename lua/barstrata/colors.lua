a7e9fa179fe4c7694fa0c1be1bdb1cea99a29d8d
w3barsi <darcybalaga@gmail.com>
2022-10-28 21:24:33 +0800

    color stuff

--- lua/barstrata/colors.lua
+++ lua/barstrata/colors.lua
@@ -46,6 +46,7 @@ M.default = {
   -- Basic
   bg = "#111319",
   bg2 = "#0D0E13",
+  bgog = "#191c25",
   fg = "#ffffff",
 
   -- Normal
@@ -55,6 +56,7 @@ M.default = {
   light_yellow = "#dcdcaa",
   orange = "#ebc88d",
   blue = "#a0b9d8",
+  blue2 = "#368AEB",
   blue3 = "#91B4D5",
   blue4 = "#7390AA",
   green = "#92c47e",
@@ -63,8 +65,52 @@ M.default = {
   teal2 = "#5FB3A1",
   teal3 = "#42675A",
   magenta = "#c6aed7",
-  light_pink = "#fabec0", -- From nexxel's website
-  -- pink = "#FCC5E9",
+  -- light_pink = "#fabec0", -- From nexxel's website
+  light_pink = "#FCC5E9",
+  bright_pink = "#f087bd",
+  bright_magenta = "#d0679d",
+
+  -- Grays
+  gray01 = "#20222d",
+  gray02 = "#272935",
+  gray03 = "#2e313d",
+  gray04 = "#3c3f4e",
+  gray05 = "#494c5a",
+  gray06 = "#6c6f82",
+  gray07 = "#85889b",
+
+  -- Special
+  urgent_red = "#fb4934",
+  urgent_yellow = "#fabd2f",
+  urgent_green = "#8ec07c",
+  urgent_blue = "#91DDF2",
+  none = "NONE",
+}
+
+M.black = {
+  -- Basic
+  bg = "#000000",
+  bg2 = "#000000",
+  fg = "#ffffff",
+
+  -- Normal
+  black = "#4b4e57",
+  red = "#fe9f7c",
+  yellow = "#d2b45f",
+  light_yellow = "#dcdcaa",
+  orange = "#ebc88d",
+  blue = "#a0b9d8",
+  blue2 = "#368AEB",
+  blue3 = "#91B4D5",
+  blue4 = "#7390AA",
+  green = "#92c47e",
+  cyan = "#7dc2c7",
+  teal = "#5de4c7", -- From poimandres
+  teal2 = "#5FB3A1",
+  teal3 = "#42675A",
+  magenta = "#c6aed7",
+  -- light_pink = "#fabec0", -- From nexxel's website
+  light_pink = "#FCC5E9",
   bright_pink = "#f087bd",
   bright_magenta = "#d0679d",
 
